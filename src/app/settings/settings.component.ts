2021d889e2455d532b7805ff5fefa5582459105a
maheshpcse <maheshmahi1599@gmail.com>
2019-12-12 18:36:08 +0530

    new changes ui

---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
+declare var $: any;
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
